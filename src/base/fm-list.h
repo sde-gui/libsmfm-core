3262fc38d0b189d9efbc80f39aaa8015f1511e97
Vadim Ushakov <igeekless@gmail.com>
2014-02-28 11:52:03 +0800

    formatting in fm-list.c and fm-list.h

--- src/base/fm-list.h
+++ src/base/fm-list.h
@@ -47,7 +47,7 @@ struct _FmList
 struct _FmListFuncs
 {
     gpointer (*item_ref)(gpointer item);
-    void (*item_unref)(gpointer item);
+    void     (*item_unref)(gpointer item);
 };
 
 FmList* fm_list_new(FmListFuncs* funcs);
@@ -61,11 +61,12 @@ void fm_list_unref(FmList* list);
  * all APIs for GQueue should be usable */
 
 void fm_list_clear(FmList* list);
-#ifndef __GTK_DOC_IGNORE__
+
 static inline gboolean fm_list_is_empty(FmList* list)
 {
     return g_queue_is_empty((GQueue*)list);
 }
+
 static inline guint fm_list_get_length(FmList* list)
 {
     return g_queue_get_length((GQueue*)list);
@@ -75,18 +76,22 @@ static inline void fm_list_reverse(FmList* list)
 {
     g_queue_reverse((GQueue*)list);
 }
+
 static inline void fm_list_foreach(FmList* list, GFunc f, gpointer d)
 {
     g_queue_foreach((GQueue*)list,f,d);
 }
+
 static inline GList* fm_list_find(FmList* list, gpointer d)
 {
     return g_queue_find((GQueue*)list,d);
 }
+
 static inline GList* fm_list_find_custom(FmList* list, gconstpointer d, GCompareFunc f)
 {
     return g_queue_find_custom((GQueue*)list,d,f);
 }
+
 static inline void fm_list_sort(FmList* list, GCompareDataFunc f, gpointer d)
 {
     g_queue_sort((GQueue*)list,f,d);
@@ -96,10 +101,12 @@ static inline void fm_list_push_head(FmList* list, gpointer d)
 {
     g_queue_push_head((GQueue*)list,(list)->funcs->item_ref(d));
 }
+
 static inline void fm_list_push_tail(FmList* list, gpointer d)
 {
     g_queue_push_tail((GQueue*)list,(list)->funcs->item_ref(d));
 }
+
 static inline void fm_list_push_nth(FmList* list, gpointer d, guint n)
 {
     g_queue_push_nth((GQueue*)list,(list)->funcs->item_ref(d),n);
@@ -109,10 +116,12 @@ static inline void fm_list_push_head_noref(FmList* list, gpointer d)
 {
     g_queue_push_head((GQueue*)list,d);
 }
+
 static inline void fm_list_push_tail_noref(FmList* list, gpointer d)
 {
     g_queue_push_tail((GQueue*)list,d);
 }
+
 static inline void fm_list_push_nth_noref(FmList* list, gpointer d, guint n)
 {
     g_queue_push_nth((GQueue*)list,d,n);
@@ -122,10 +131,12 @@ static inline gpointer fm_list_pop_head(FmList* list)
 {
     return g_queue_pop_head((GQueue*)list);
 }
+
 static inline gpointer fm_list_pop_tail(FmList* list)
 {
     return g_queue_pop_tail((GQueue*)list);
 }
+
 static inline gpointer fm_list_pop_nth(FmList* list, guint n)
 {
     return g_queue_pop_nth((GQueue*)list,n);
@@ -135,10 +146,12 @@ static inline gpointer fm_list_peek_head(FmList* list)
 {
     return g_queue_peek_head((GQueue*)list);
 }
+
 static inline gpointer fm_list_peek_tail(FmList* list)
 {
     return g_queue_peek_tail((GQueue*)list);
 }
+
 static inline gpointer fm_list_peek_nth(FmList* list, guint n)
 {
     return g_queue_peek_nth((GQueue*)list,n);
@@ -148,19 +161,20 @@ static inline gint fm_list_index(FmList* list, gconstpointer d)
 {
     return g_queue_index((GQueue*)list,d);
 }
-#endif /* __GTK_DOC_IGNORE__ */
 
 void fm_list_remove(FmList* list, gpointer data);
 void fm_list_remove_all(FmList* list, gpointer data);
-#ifndef __GTK_DOC_IGNORE__
+
 static inline void fm_list_insert_before(FmList* list, GList* s, gpointer d)
 {
     g_queue_insert_before((GQueue*)list,s,(list)->funcs->item_ref(d));
 }
+
 static inline void fm_list_insert_after(FmList* list, GList* s, gpointer d)
 {
     g_queue_insert_after((GQueue*)list,s,(list)->funcs->item_ref(d));
 }
+
 static inline void fm_list_insert_sorted(FmList* list, gpointer d, GCompareDataFunc f, gpointer u)
 {
     g_queue_insert_sorted((GQueue*)list,(list)->funcs->item_ref(d),f,u);
@@ -170,10 +184,12 @@ static inline void fm_list_insert_before_noref(FmList* list, GList* s, gpointer 
 {
     g_queue_insert_before((GQueue*)list,s,d);
 }
+
 static inline void fm_list_insert_after_noref(FmList* list, GList* s, gpointer d)
 {
     g_queue_insert_after((GQueue*)list,s,d);
 }
+
 static inline void fm_list_insert_sorted_noref(FmList* list, gpointer d, GCompareDataFunc f, gpointer u)
 {
     g_queue_insert_sorted((GQueue*)list,d,f,u);
@@ -183,10 +199,12 @@ static inline void fm_list_push_head_link(FmList* list, GList* l_)
 {
     g_queue_push_head_link((GQueue*)list,l_);
 }
+
 static inline void fm_list_push_tail_link(FmList* list, GList* l_)
 {
     g_queue_push_tail_link((GQueue*)list,l_);
 }
+
 static inline void fm_list_push_nth_link(FmList* list, guint n, GList* l_)
 {
     g_queue_push_nth_link((GQueue*)list,n,l_);
@@ -196,10 +214,12 @@ static inline GList* fm_list_pop_head_link(FmList* list)
 {
     return g_queue_pop_head_link((GQueue*)list);
 }
+
 static inline GList* fm_list_pop_tail_link(FmList* list)
 {
     return g_queue_pop_tail_link((GQueue*)list);
 }
+
 static inline GList* fm_list_pop_nth_link(FmList* list, guint n)
 {
     return g_queue_pop_nth_link((GQueue*)list,n);
@@ -209,10 +229,12 @@ static inline GList* fm_list_peek_head_link(FmList* list)
 {
     return g_queue_peek_head_link((GQueue*)list);
 }
+
 static inline GList* fm_list_peek_tail_link(FmList* list)
 {
     return g_queue_peek_tail_link((GQueue*)list);
 }
+
 static inline GList* fm_list_peek_nth_link(FmList* list, guint n)
 {
     return g_queue_peek_nth_link((GQueue*)list,n);
@@ -222,15 +244,17 @@ static inline gint fm_list_link_index(FmList* list, GList* l_)
 {
     return g_queue_index((GQueue*)list,l_);
 }
+
 static inline void fm_list_unlink(FmList* list, GList* l_)
 {
     g_queue_unlink((GQueue*)list,l_);
 }
+
 static inline void fm_list_delete_link_nounref(FmList* list, GList* l_)
 {
     g_queue_delete_link((GQueue*)list,l_);
 }
-#endif /* __GTK_DOC_IGNORE__ */
+
 void fm_list_delete_link(FmList* list, GList* l_);
 
 G_END_DECLS
